beab8e61966c2274d0a56f25aba7f976427b5c53
meihuawu <meihuawu@umich.edu>
2015-07-10 18:21:32 -0700

    update format and better probability

--- scenario1.py
+++ scenario1.py
@@ -12,10 +12,10 @@ nn_clf = Classifier(
         Layer("Sigmoid", units=p, weight_decay=0.001),
         Layer("Softmax")],
     learning_rate=0.01,
-    n_iter=2)
+    n_iter=500)
 
 gini_clf = tree.DecisionTreeClassifier()
 entropy_clf = tree.DecisionTreeClassifier(criterion="entropy")
 
-comparator = Comparator(s, nn_clf, gini_clf, entropy_clf, 2)
+comparator = Comparator(s, nn_clf, gini_clf, entropy_clf, 100)
 comparator.compare()
